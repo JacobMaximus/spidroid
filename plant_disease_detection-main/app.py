e8e6240de3f4b3b3ebf64920eda6d50e808f483f
JacobMaximus <178012512+JacobMaximus@users.noreply.github.com>
2025-03-27 16:37:50 +0530

    added plant ai detection to git properly

---
+++ plant_disease_detection-main/app.py
@@ -0,0 +1,62 @@
+import os
+import tensorflow as tf
+import numpy as np
+# from tensorflow.keras.preprocessing import image
+from keras._tf_keras.keras.preprocessing import image
+from PIL import Image
+import cv2
+# from keras.models import load_model
+from keras._tf_keras.keras.models import load_model
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+# from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras._tf_keras.keras.preprocessing.image import load_img, img_to_array
+app = Flask(__name__)
+
+model =load_model('model.h5')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+labels = {0: 'Healthy', 1: 'Powdery', 2: 'Rust'}
+
+
+def getResult(image_path):
+    img = load_img(image_path, target_size=(225,225))
+    x = img_to_array(img)
+    x = x.astype('float32') / 255.
+    x = np.expand_dims(x, axis=0)
+    predictions = model.predict(x)[0]
+    return predictions
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        # basepath = os.path.dirname(__file__)
+        # file_path = os.path.join(
+        #     basepath, 'uploads', secure_filename(f.filename))
+        # f.save(file_path)
+        basepath = os.path.dirname(__file__)
+        upload_folder = os.path.join(basepath, 'uploads')
+
+        # ✅ Ensure the uploads folder exists
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+
+        file_path = os.path.join(upload_folder, secure_filename(f.filename))
+        f.save(file_path)
+
+        predictions=getResult(file_path)
+        predicted_label = labels[np.argmax(predictions)]
+        return str(predicted_label)
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
